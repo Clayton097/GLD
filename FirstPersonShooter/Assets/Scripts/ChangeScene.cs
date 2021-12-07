fcf74b701f17b6ea25ba2e61dba983dbc1eebf76
ClaytonPace <clayton.pace.a101002@mcast.edu.mt>
2021-12-07 18:19:02 +0100

    Creating Main Menu

---
+++ FirstPersonShooter/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    //Load first Scene
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    //Quits the game
+    public void ExitGame()
+    {
+        Debug.Log("Game Quit");
+        Application.Quit();
+    }
+
+    //Used to change scene to load Main Menu
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+}
