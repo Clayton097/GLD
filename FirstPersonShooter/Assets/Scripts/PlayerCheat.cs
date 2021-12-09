5b73642ee46dca75b02c9332c8f21fbffd50d45f
ClaytonPace <clayton.pace.a101002@mcast.edu.mt>
2021-12-09 17:54:22 +0100

    Player Cheat On KeyDown "P" destroy Player

---
+++ FirstPersonShooter/Assets/Scripts/PlayerCheat.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCheat : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Self Destroy
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
